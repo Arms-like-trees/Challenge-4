7f291170aae82c8259b50994cecf801d3562b9e9
Arms-like-trees <117699558+Arms-like-trees@users.noreply.github.com>
2022-12-13 22:25:29 -0600

    adds timer portion to Javascript

---
+++ Assets/js/script.js
@@ -0,0 +1,18 @@
+// For setting the timer
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 75;
+
+function setTime() {
+    var timerInterval = setInterval(function(){
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if(secondsLeft === 0){
+        clearInterval(timerInterval);
+
+    }
+}, 1000);
+}
+
+setTime ();
