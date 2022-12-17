72baa53c8b9c24c9aafcd9f0a01bf8b918616f4c
Arms-like-trees <117699558+Arms-like-trees@users.noreply.github.com>
2022-12-17 10:50:40 -0600

    start working on the local storage aspect

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,7 @@
+var questionSet = document.querySelector("#questionSet")
+var questionDivs = document.querySelectorAll('#questionSet > div')
+var startQuiz = document.querySelector('.start-box')
+
 // For setting the timer
 var timeEl = document.querySelector(".time");
 
@@ -21,9 +25,13 @@ function setTime() {
 //The start button to start the quiz
 var startTimer = document.querySelector("#startButton");
 
-startTimer.addEventListener("click", setTime)
+startTimer.addEventListener("click", setTime);
 
 
+
+
+
+// To change the divs
 var currentQuestion = 0;
 document.querySelector("#questionSet").addEventListener('click', function(event){
     var targetEl = event.target;
@@ -32,27 +40,68 @@ document.querySelector("#questionSet").addEventListener('click', function(event)
     if (targetEl.matches("button")) {
         var questionDivs = document.querySelectorAll('#questionSet > div');
         currentQuestion += parseInt(forward,  10);
+        if (forward <= 5){
+            console.log(currentQuestion)
         questionDivs[currentQuestion].classList.remove('text-box');
         questionDivs[currentQuestion - 1].classList.add("text-box")
+        } if (secondsLeft <= 0){
+            secondsLeft = 0;
+            questionDivs[4].classList.add('text-box');
+            questionDivs[currentQuestion].classList.remove('text-box')
+        }
+        
     }
 })
 
 
-// For selecting the wrong answer
+// For selecting the wrong answer basic code
+/*
+ var incorrect = document.querySelector(".wrong");
 
-var incorrect = document.querySelector(".wrong");
+ incorrect.addEventListener("click", function(){
 
-incorrect.addEventListener("click", function(){
-
-    secondsLeft-=10;
-})
+     secondsLeft-=10;
+ })
 
 
-// Stop timer
+// Stop timer basic code
 
 var stopTimer = document.querySelector('.stop');
-
+console.log(stopTimer)
 stopTimer.addEventListener('click', function(){
     clearInterval(timerInterval);
 })
+*/
 
+
+
+
+
+//Combined function of wrong answer and stop timer.
+questionSet.addEventListener("click", function(event) {
+    var element = event.target;
+    if (element.matches('.stop')) {
+        clearInterval(timerInterval);
+    } if (element.matches('.wrong')){
+        secondsLeft-=10
+    }
+})
+
+
+//Submit score to local storage
+
+var score = document.querySelector("#score");
+var submitScore = document.querySelector("#submitInitials");
+var initials = document.querySelector('Initials').value;
+score.textContent = timerInterval//how do i grab the time remaining to set as time?
+
+submitScore.addEventListener('click',function(event){
+    event.preventDefault();
+    if (initials === "") {
+        displayMessage("error", "Initials cannot be blank")
+    } else {
+        displayMessage("success", "Your score was saved");
+        localStorage.setItem('initials', initials)
+        //place for score
+    }
+})
